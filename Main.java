fa03cf34f0ac06de3735c21436f5a724868a6624
Jasper <jasper1196@gmail.com>
2018-01-09 12:26:27 +0100

    Stage is set

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
-public class Main {
-  public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  public void start(Stage mainStage) {
+    mainStage.setTitle("Phase 3");
+    BorderPane root = new BorderPane();
+    Scene scene = new Scene(root, 1030, 750);
+    mainStage.setScene(scene);
+    mainStage.show();
   }
 }
