7d17f8d9ca5e708ef2ac0b6b06433e7fc81003cd
Noixas\aCmRz <acmrz@outlook.com>
2018-01-09 14:29:26 +0100

    Abstrcat algorithm finished

--- Algorithm.java
+++ Algorithm.java
@@ -1,6 +1,36 @@
+import java.util.*;
+import java.util.LinkedList;
+
 public abstract class Algorithm{
-  public Algorithm()
+  protected boolean _done = false;
+  protected List<SolutionSet> _solutions = new LinkedList<SolutionSet>();
+  /**
+   * Start computing solutions separated from constructor to be able to configure it with the UI
+   * and use Star button from UI
+   */
+  public abstract void Start();
+  /**
+   * Check if the Algorithm has finished
+   * @return true if algorithm has ended
+   */
+  public boolean isDone()
   {
-  
+    return _done;
+  }
+  /**
+   * Get list of all SolutionSet found
+   * @return [list of solutionSets]
+   */
+  public List<SolutionSet> getSolutions()
+  {
+    return _solutions;
+  }
+  /**
+   * Add a solutionSet to the total solution found by this algorithm
+   * @param SolutionSet pSolution [the new solution to be added]
+   */
+  public void AddSolution(SolutionSet pSolution)
+  {
+    _solutions.add(pSolution);
   }
 }
