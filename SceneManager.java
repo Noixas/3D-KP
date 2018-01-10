fd741894b653870acf28fc4065f598b39671fbfa
Jasper <jasper1196@gmail.com>
2018-01-10 12:50:04 +0100

    Scenemanager and sceneChange work

--- SceneManager.java
+++ SceneManager.java
@@ -4,20 +4,28 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 
 public class SceneManager {
+  private static Stage stage;
   private static Scene menuScene;
   private static Scene worldScene;
 
-  public SceneManager(Stage stage){
+  public SceneManager(Stage mainStage){
     BorderPane root = new BorderPane();
     Group worldGroup = new Group();
     menuScene = new Scene(root, 1040, 750);
     worldScene = new Scene(worldGroup, 1040, 750);
-    stage.setScene(menuScene);
+    stage = mainStage;
+    mainStage.setScene(menuScene);
     MenuUI menu = new MenuUI(root);
     WorldUI world = new WorldUI(worldGroup);
   }
 
   public static void changeScene(int i) {
-    
+
+    if (i == 1) {
+      stage.setScene(menuScene);
+    }
+    else if (i == 2) {
+      stage.setScene(worldScene);
+    }
   }
 }
