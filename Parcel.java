059288854614cabb1996a498721825703c81df10
Jasper <jasper1196@gmail.com>
2018-01-19 15:56:31 +0100

    is guuud now

--- Parcel.java
+++ Parcel.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Parcel{
   protected Vector3D _position;
   protected Vector3D _size;
@@ -63,69 +62,6 @@ public class Parcel{
     else if(_position.y > v.y || v.y >= _position.y + _size.y) return false;
     else if(_position.z > v.z || v.z >= _position.z + _size.z) return false;
     else return true;
-=======
-public class Parcel {
-protected Vector3D _position;
-protected Vector3D _size;
-protected double _value;
-protected double _volume;
-protected double _densityValue;
-protected boolean _invisible = false;
-public Parcel(Vector3D pSize, double pValue)
-{
-        _size = pSize;
-        _value = pValue;
-        _volume = _size.x*_size.y*_size.z;
-        _densityValue = _value / _volume;
-}
-public void setPosition(Vector3D newPos)
-{
-        _position = newPos;
-}
-public Vector3D getPosition()
-{
-        return _position;
-}
-public void setSize(Vector3D newSize)
-{
-        _size = newSize;
-}
-public Vector3D getSize()
-{
-        return _size;
-}
-public double getValue()
-{
-        return _value;
-}
-public double getVolume()
-{
-        return _volume;
-}
-public void setInvisible(boolean b)
-{
-        _invisible = b;
-}
-public boolean getInvisible()
-{
-        return _invisible;
-}
-public double getDensityValue()
-{
-        return _densityValue;
-}
-/**
- * Check if a vector v is inside the parcel
- * @param  Vector3D v             Vector to be checked
- * @return          [True if the vector point is inside the parcel]
- */
-public boolean contains(Vector3D v)
-{
-        if(_position.x > v.x || v.x >= _position.x + _size.x) return false;
-        else if(_position.y > v.y || v.y >= _position.y + _size.y) return false;
-        else if(_position.z > v.z || v.z >= _position.z + _size.z) return false;
-        else return true;
->>>>>>> fc5fa77f2d7d78da8da518204c6aa44cdca10e57
 
 }
 /**
