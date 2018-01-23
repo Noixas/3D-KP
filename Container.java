6986e02c4c54a9750a0f9785559449a71c804515
Giacomo Anerdi <giacomo.anerdi@gmail.com>
2018-01-23 15:15:24 +0100

    Container javadoc done

--- Container.java
+++ Container.java
@@ -1,4 +1,10 @@
+/**
+ * Class that represents the container.
+ */
 public class Container extends Parcel{
+  /**
+   * Container's constructor.
+   */
   public Container()
   {
     super(new Vector3D(16.5,2.5,4.0),0);
